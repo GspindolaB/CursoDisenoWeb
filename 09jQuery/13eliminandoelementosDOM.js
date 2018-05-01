a806b57bb9369f9efc7d884b98e24ae6e2662ef4
Guillermo Spindola <gspindolab@gmail.com>
2018-04-30 20:38:05 -0500

    Eliminando elementos del DOM

---
+++ 09jQuery/13eliminandoelementosDOM.js
@@ -0,0 +1,8 @@
+$(function(){
+
+    //remove() Elimina el elemento padre y sus elementos hijos
+    //$('#contenedor').remove();
+    //$('#contenedor').children().remove();
+    //$('#contenedor').children('#segunda').remove();
+    $('#contenedor').empty();
+});
